c41c9aaf9f9e5fd5feffb09b1a3902b3cbc20616
zomalala <zomalala.clement@gmail.com>
2021-04-01 17:52:13 +0200

    cercle

---
+++ CCercle.py
@@ -0,0 +1,22 @@
+#!/usr/local/python3.9
+
+#-*-coding:UTF-8-*-
+
+#définition de la classe Cercle
+class Cercle:  
+
+#méthode constructeur
+    def __init__(self, rayon):
+        self.rayon=float(input("Donner le rayon de votre cercle en cm: "))
+
+#méthode permettant de calculer l'aire d'un cercle
+    def aire(self,aire_cercle):
+        self.aire_cercle=float(pi*rayon*rayon)
+
+#méthode permettant de calculer le périmètre d'un cercle
+
+    def perimetre(self,perimetre_cercle):
+        self.perimetre_cercle=float(pi*rayon*2)
+
+
+        
